be90acc26f3aeca08d2ca3ec7a102bf858e837e6
Jessica Lewinter <jessicalewinter@gmail.com>
2020-08-02 22:55:47 -0300

    Water app projects created

---
+++ animations/WaterReminderSwiftUI/WaterReminderSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterReminderSwiftUI
+//
+//  Created by Jessica Lewinter on 02/08/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ animations/WaterReminderUIKit/WaterReminderUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterReminderUIKit
+//
+//  Created by Jessica Lewinter on 02/08/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
