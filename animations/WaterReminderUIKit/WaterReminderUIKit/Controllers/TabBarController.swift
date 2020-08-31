5f49699582a7cb27c1bf3737c7bb9c08b331dbe3
Jessica Lewinter <jessicalewinter@gmail.com>
2020-08-31 14:48:01 -0300

    Add tababr images

--- animations/WaterReminderUIKit/WaterReminderUIKit/Controllers/TabBarController.swift
+++ animations/WaterReminderUIKit/WaterReminderUIKit/Controllers/TabBarController.swift
@@ -12,17 +12,37 @@ class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tabBar.invalidateIntrinsicContentSize()
+        self.selectedImage(with: .blue)
         instantiateControllers()
     }
     
     func instantiateControllers() {
         let dailyController = UINavigationController(rootViewController: DailyViewController())
-        dailyController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        dailyController.tabBarItem = UITabBarItem(title: "Daily", image: UIImage(named: "water-glass"), tag: 0)
         
         let reportController = UINavigationController(rootViewController: ReportViewController())
-        reportController.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 1)
+        reportController.tabBarItem = UITabBarItem(title: "Report", image: UIImage(named: "graph"), tag: 1)
         
         let controllers = [dailyController, reportController]
         viewControllers = controllers
+    }
+}
+
+import UIKit
+
+extension UITabBarController {
+    func selectedImage(with color: UIColor) {
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.barStyle = .black
+        self.tabBar.tintColor = .black
+        self.tabBar.barTintColor = .white
+        
+        self.tabBar.layer.shadowColor = UIColor.lightGray.cgColor
+        self.tabBar.layer.shadowOffset = CGSize(width: 0.0, height: 4.0)
+        self.tabBar.layer.shadowRadius = 10
+        self.tabBar.layer.shadowOpacity = 1
+        self.tabBar.layer.masksToBounds = false
+        
     }
 }
